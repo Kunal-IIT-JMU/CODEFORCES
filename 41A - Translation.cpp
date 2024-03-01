52e5ec646c82bb9517c39d6d723d3a98c734c617
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-03-01 19:34:45 +0530

    ok

---
+++ 41A - Translation.cpp
@@ -0,0 +1,17 @@
+//Kunal Sharma IIT JAMMU
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s1, s2;
+    cin >> s1 >> s2;
+    reverse(s1.begin(),s1.end());
+    if(s1 == s2){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+    return 0;
+}
