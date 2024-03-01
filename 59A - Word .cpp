0e955972575b9d3cc8f9c2ce72ba6639a0735ec9
Kunal Sharma <159030119+Kunal-IIT-JMU@users.noreply.github.com>
2024-03-01 18:54:39 +0530

    ok

---
+++ 59A - Word .cpp
@@ -0,0 +1,27 @@
+//Kunal Sharma IIT JAMMU
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    int up=0,low=0;
+    cin >> s;
+    for(int i =0; i< s.length(); i++){
+        if(isupper(s[i])){
+            up++;
+        }
+        else{
+            low++;
+        }
+    }
+    
+    if(up > low){
+        transform(s.begin(),s.end(),s.begin(),::toupper);
+    }
+    else{
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+    }
+    cout << s;
+    return 0;
+}
